e573784744a728ac6ba9c847d2ab2109c38094fe
carlosq <cquintov@miumg.edu.gt>
2025-10-08 22:44:50 -0600

    finalizando la funcionalidad login

--- src/app/layout/menu/menu.ts
+++ src/app/layout/menu/menu.ts
@@ -3,7 +3,7 @@ import { Component, EventEmitter, Output} from '@angular/core';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 
 
 @Component({
@@ -19,10 +19,27 @@ import { RouterModule } from '@angular/router';
 
 export class Menu {
 
+  loggin: boolean=true;
+
+
   @Output () toggleSidenav = new EventEmitter<void>();
 
   onToggleSidenav(){
     this.toggleSidenav.emit();
+  }
+
+  constructor(private router: Router) {
+
+  }
+
+  isLogedIn(){
+    return this.loggin;
+  }
+
+  loginLogout(){
+    if(this.isLogedIn()){
+      this.router.navigate(['/login']);
+    }
   }
 
 }
